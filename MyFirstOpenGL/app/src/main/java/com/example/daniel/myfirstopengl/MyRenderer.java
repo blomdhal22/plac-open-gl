598541762cb3082e0493ae50518e3ef3e85de13e
blomdhal22 <blomdhal22@gmail.com>
2018-06-03 23:11:00 +0900

    MyFirstOpenGL

---
+++ MyFirstOpenGL/app/src/main/java/com/example/daniel/myfirstopengl/MyRenderer.java
@@ -0,0 +1,33 @@
+package com.example.daniel.myfirstopengl;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+public class MyRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set the background clear color to red. The first component is
+        // red, the second is green, the third is blue, and the last
+        // component is alpha, which we don't use in this lesson.
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Set the OpenGL viewport to fill the entire surface.
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear the rendering surface.
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
